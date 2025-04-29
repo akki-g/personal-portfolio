5734ee2b9cbe179eb091852b667058d07c414243
akki <akshat.guduru@gmail.com>
2025-04-29 17:12:24 -0400

    test

--- wed-front/src/pages/About.jsx
+++ wed-front/src/pages/About.jsx
@@ -1,7 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
 import './About.css';
-import axios from 'axios';
 import './Experiences';
 import apiClient from './AxiosInstance';
 import Experiences from './Experiences';
